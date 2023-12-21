59edb854f6349eba95a2b82df825fbbe5df7a7cc
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-12-21 19:39:53 +0200

    second

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,16 +2,11 @@ import css from "./FriendList.moduls.css"
 export default function FriendList ({friends}){
     return (
       <ul className="friend-list">
-        {friends.map(friend => (
-          <li className="item" key={friend.id}>
-            <span className="status"></span>
-            <img
-              className="avatar"
-              src={friend.avatar}
-              alt="User avatar"
-              width="48"
-            />
-            <p className="name">{friend.name}</p>
+        {friends.map(({ id, avatar, name, isOnline }) => (
+          <li className="item" key={id}>
+            <span event={isOnline.toString()}></span>
+            <img className="avatar" src={avatar} alt="User avatar" width="48" />
+            <p className="name">{name}</p>
           </li>
         ))}
       </ul>
